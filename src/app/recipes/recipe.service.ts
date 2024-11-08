5c3c77955ec686713a773f73ccd3848a4416af2b
Ronney Vargas <ronneyvargas@gmail.com>
2024-11-08 12:55:24 -0600

    Updated the look and added a default recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,51 +9,30 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    // private recipes: Recipe[] = [
-    //     new Recipe(
-    //         'Tasty Schnitzal',
-    //         'A super-tasty Scnitzal - just awesome!',
-    //         'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-    //         [
-    //             new Ingredient('Meat', 1),
-    //             new Ingredient('French Fries', 20),
-    //             new Ingredient('Lemons', 2)
-    //         ]),
-    //     new Recipe(
-    //         'Big Fat Burger',
-    //         'What else you need to say?',
-    //         'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-    //         [
-    //             new Ingredient('Buns', 2),
-    //             new Ingredient('Meat', 1),
-    //             new Ingredient('Lettuce', 1),
-    //             new Ingredient('Tomato', 3),
-    //             new Ingredient('Pickles', 4)
-    //         ]),
-    //     new Recipe(
-    //         'Yummy French Toast',
-    //         'Perfect in the morning or at any time!',
-    //         './../../assets/images/french_toast.jpeg',
-    //         [
-    //             new Ingredient('Bread', 2),
-    //             new Ingredient('banana', 1),
-    //             new Ingredient('Blue Berries', 10),
-    //             new Ingredient('Syrup', 10),
-    //             new Ingredient('Powdered Sugar', 1)
-    //         ]),
-    //     new Recipe(
-    //         'Slurpee Spaghetti',
-    //         'Enjoy a tasty of Italy',
-    //         './../../assets/images/spaghetti.jpeg',
-    //         [
-    //             new Ingredient('Spaghetti', 4),
-    //             new Ingredient('Spaghetti Sauce', 1),
-    //             new Ingredient('Italian Meat Balls', 4),
-    //             new Ingredient('Garlic Bread', 2)
-    //         ])
-    // ];
-
-    private recipes: Recipe[] = [];
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Classic Tomato Soup',
+            'Homemade tomato soup with simple, fresh ingredients. Perfect comfort food!',
+            'assets/images/Classic_tomotoe_soup.png',
+            [
+                new Ingredient('Roma Tomatoes', 3),
+                new Ingredient('Yellow Onion', 1),
+                new Ingredient('Carrots', 2),
+                new Ingredient('Celery Stalks', 2),
+                new Ingredient('Garlic Cloves', 3),
+                new Ingredient('Chicken Stock', 4),
+                new Ingredient('Heavy Cream', 1),
+                new Ingredient('Fresh Basil', 1)
+            ],
+            `1. Saute vegetables: Heat olive oil in a pot over medium heat. Add chopped onion, carrots, and celery. Cook until softened (8-10 mins).
+                2. Add aromatics: Add minced garlic and cook for 1 minute until fragrant.
+                3. Add tomatoes: Add diced tomatoes with their juice. Cook 8-10 mins until tomatoes soften.
+                4. Add liquids: Pour in chicken stock and bring to a boil. Reduce heat, cover and simmer 20-25 mins.
+                5. Blend: Use an immersion blender to puree until smooth.
+                6. Finish: Stir in heavy cream and chopped fresh basil. Season with salt and pepper to taste.`
+        )
+        // ... you can keep or remove other existing recipes ...
+    ];
 
     constructor(private slService: ShoppingListService) {}
 
@@ -81,6 +60,7 @@ export class RecipeService {
 
     updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     deleteRecipe(index: number) {
